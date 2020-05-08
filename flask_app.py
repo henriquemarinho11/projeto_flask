21fc1f8e0768b525acb2fbdde50582fc96b29dd4
Henrique Marinho <henriquemarinho11@hotmail.com>
2020-05-08 16:07:26 -0300

    adicionando um inicio alternativo

--- flask_app.py
+++ flask_app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def inicio():
     return render_template('index.html')
 
-@app.route('/inicio-alternativo')
+@app.route('/inicio2')
 def outro_inicio():
     return render_template('alternative_index.html')
+
+## Para rodae o projeto em desenvolvimento
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
